be37fbfac6518f9e472b45ce97df7c5da9a5f51b
than4tos <time4silence@gmail.com>
2018-02-06 17:35:21 +0700

    Add files via upload

--- song.py
+++ song.py
@@ -13,12 +13,13 @@ class Song:
         Args:
             title (str) = initialize title attribute,
             artiest (Artist) = an artiest object representing object creator
-            duration (int) = default 0 if not specified, 
+            duration (int) = default 0 if not specified,
                 initialize duration attribute
         """
         self.title = title
         self.artist = artist
         self.duration = duration
+
 
 class Album:
     """ Album class to define album attribute
@@ -36,7 +37,7 @@ class Album:
     def __init__(self, name, year, artist=None):
         self.name = name
         self.year = year
-        if self.artist is None:
+        if artist is None:
             self.artist = Artist("Various Artist")
         else:
             self.artist = artist
@@ -56,13 +57,14 @@ class Album:
         else:
             self.track.insert(position, song)
 
+
 class Artist:
     """ Artist class to store artist detail
 
     Attributes:
         name (str): name of the artist
         albums (list[Album]): a list containing album related to the artist.
-            the list include album in this collection and not an exhaustive list 
+            the list include album in this collection and not an exhaustive list
             of artist's published album
 
     method:
@@ -71,7 +73,7 @@ class Artist:
     def __init__(self, name):
         self.name = name
         self.albums = []
-    
+
     def add_album(self, album):
         """ method to add album related to artist to album list
 
@@ -80,3 +82,62 @@ class Artist:
                 if the album already present it will not add again (not yet implemented)
         """
         self.albums.append(album)
+
+
+def load_data():
+    new_artist = None
+    new_album = None
+    artist_list = []
+
+    with open("C:\\Users\\ario.bramasto\\Documents\\Temp\\Playground\\Section-12\\albums.txt", "r") as albums:
+        for line in albums:
+            # data consist of (artist, album, year, song)
+            artist_field, album_field, year_field, song_field = tuple(line.strip("\n").split("\t"))
+            year_field = int(year_field)
+            print("{}:{}:{}:{}".format(artist_field, album_field, year_field, song_field))
+
+            if new_artist is None:
+                new_artist = Artist(artist_field)
+            elif new_artist.name != artist_field:
+                # we've just read details for new artist
+                # store current album in current artist collection then create new artist object
+                new_artist.add_album(new_album)
+                artist_list.append(new_artist)
+                new_artist = Artist(artist_field)
+                new_album = None
+
+            if new_album is None:
+                new_album = Album(album_field, year_field, new_artist)
+            elif new_album.name != album_field:
+                # we've just read new album for current artist
+                # store current album in the artist's collection then create new album object
+                new_artist.add_album(new_album)
+                new_album = Album(album_field, year_field, new_artist)
+
+            # create new song object and add it to current album collection
+            new_song = Song(song_field, new_artist)
+            new_album.add_song(new_song)
+
+        # after reach the end of text file, add artist that haven't
+        # been stored - process them now
+        if new_artist is not None:
+            if new_album is not None:
+                new_artist.add_album(new_album)
+            artist_list.append(new_artist)
+
+    return artist_list
+
+
+def create_checkfile(artist_list):
+    """create check file from object data as comparison with original file"""
+    with open("C:\\Users\\ario.bramasto\\Documents\\Temp\\Playground\\Section-12\\checkfile.txt", "w") as checkfile:
+        for new_artist in artist_list:
+            for new_album in new_artist.albums:
+                for new_song in new_album.track:
+                    print("{0.name}\t{1.name}\t{1.year}\t{2.title}".format(new_artist, new_album, new_song),
+                        file=checkfile)
+
+
+if __name__ == '__main__':
+    artists = load_data()
+    create_checkfile(artists)
